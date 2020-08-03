f58794ecb0f7af0f5d5e07380c610715d463aa0f
gaoyu <gaoyu@126.com>
2020-08-03 18:30:53 +0800

    配置Druid连接池与Mybatis

---
+++ src/test/java/com/yum/oa/YumOaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yum.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumOaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
